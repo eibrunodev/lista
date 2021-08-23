d21832ca80eb1e9faf333c74935222f6e46e718e
bruno souza lima <eibrunodev@gmail.com>
2021-08-22 22:56:27 -0300

    Consumo de Api em lista

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useEffect, useState} from 'react';
+import './App.css';
+import '../src/styles/styles.css'
 function App() {
+
+  const [initialRepos, setInitialRepos] = useState([])
+  const [Repos, setRepos] = useState([]);
+
+  useEffect(()=>{
+    const fetchRepositories = async () => {
+      try {
+        const response = await fetch('https://api.github.com/users/eibrunodev/repos')
+        const data = await response.json()
+        setRepos(data)
+        setInitialRepos(data)
+        console.log(data)
+      }catch(erro){
+        console.log(erro)
+      }
+    }
+     fetchRepositories() 
+  },[])
+
+  const handle = ({target})=>{
+    if(!target.value){
+      setRepos(initialRepos)
+      return
+    }
+
+    const filterRepos = Repos.filter(({name}) => name.includes(target.value))
+
+    setRepos(filterRepos)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="Container-input">
+         <input type='text' onChange={handle} />
+       </div>
+
+        <div className="Container-list">
+            <ul className='List-containers'>
+              {Repos.map((repo)=>(
+                <li key={repo.id} > {repo.name} </li>
+              ))}
+            </ul>
+        </div>
+
+    
     </div>
   );
 }
